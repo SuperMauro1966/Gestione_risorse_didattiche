cba729895948fe96fe6351344f1e55571099f534
Ilaria <sergiilaria06@gmail.com>
2024-02-13 15:02:39 +0100

    Aggiunti i drop table

--- db/risorse_didattiche.sql
+++ db/risorse_didattiche.sql
@@ -1,4 +1,17 @@
 BEGIN TRANSACTION;
+
+DROP TABLE IF EXISTS tb_corso;
+DROP TABLE IF EXISTS tb_elenco_risorse_disponibili;
+DROP TABLE IF EXISTS tb_argomento;
+DROP TABLE IF EXISTS tb_studente_corso;
+DROP TABLE IF EXISTS tb_tutor_argomento;
+DROP TABLE IF EXISTS tb_studente_corso_facoltativo;
+DROP TABLE IF EXISTS tb_risorsa_argomento;
+DROP TABLE IF EXISTS tb_risorsa_digitale;
+DROP TABLE IF EXISTS tb_risorsa_fisica;
+DROP TABLE IF EXISTS tb_studente;
+DROP TABLE IF EXISTS tb_tutor;
+
 CREATE TABLE IF NOT EXISTS "tb_corso" (
 	"ID_corso"	INTEGER,
 	"Nome"	VARCHAR(30),
@@ -87,21 +100,6 @@ CREATE TABLE IF NOT EXISTS "tb_tutor" (
 INSERT INTO "tb_corso" ("ID_corso","Nome","Anno_corso") VALUES (1,'ProvaCorso1',1),
  (2,'ProvaCorso2',2),
  (3,'ProvaCorso3',3);
-<<<<<<< HEAD
-INSERT INTO "tb_studente_corso" ("id_corso ","matricola","classe","indirizzo") VALUES (1,1,'classe1','indirizzo1'),
- (2,2,'classe2','indirizzo2'),
- (3,3,'classe3','indirizzo3');
-INSERT INTO "tb_tutor_argomento" ("id_argomenti","id_tutor") VALUES (NULL,NULL);
-INSERT INTO "tb_studente_corso_facoltativo" ("id_corso","matricola") VALUES (1,1),
- (2,2),
- (3,3);
-INSERT INTO "tb_studente" ("Matricola","Nome","Cognome","mail_istituzionale","Data_nascita","Anno_corso","Lingua_scelta","Password") VALUES (1,'Studente1','Cognome1','mail1','01/01/01',1,'lingua1','password1'),
- (2,'Studente2','Cognome2','mail2','02/02/02',2,'lingua2','password2'),
- (3,'Studente3','Cognome3','mail3','03/03/03',3,'lingua3','password3');
-INSERT INTO "tb_tutor" ("ID_tutor","Nome","Cognome","mail_interna","Password","Admin","Attivo","id_risorse") VALUES (NULL,'Tutor1','Cognome1','mail1','password1','true','true',1),
- (NULL,'Tutor2','Cognome2','mail2','password2','false','true',2),
- (NULL,'Tutor3','Cognome3','mail3','password3','false ','false',3);
-=======
 INSERT INTO "tb_elenco_risorse_disponibili" ("ID_risorse","Tipo ","matricola","id_tutor") VALUES (1,'tipo1',1,1),
  (2,'tipo2',2,2),
  (3,'tipo3',3,3);
@@ -117,5 +115,17 @@ INSERT INTO "tb_risorsa_digitale" ("ID_digitale","Nome","Link","Approvata","id_r
 INSERT INTO "tb_risorsa_fisica" ("ID_fisiche","Nome","Ubicazione","Approvata","Tipo","id_risorse") VALUES (1,'nome1','ubicazione1','true','tipo1',1),
  (2,'nome2','ubicazione2','false','tipo2',2),
  (3,'nome3','ubicazione3','true','tipo3',3);
->>>>>>> 0bad4e38a6e0e380f715d75626bdd5fbc02eada6
+INSERT INTO "tb_studente_corso" ("id_corso ","matricola","classe","indirizzo") VALUES (1,1,'classe1','indirizzo1'),
+ (2,2,'classe2','indirizzo2'),
+ (3,3,'classe3','indirizzo3');
+INSERT INTO "tb_tutor_argomento" ("id_argomenti","id_tutor") VALUES (NULL,NULL);
+INSERT INTO "tb_studente_corso_facoltativo" ("id_corso","matricola") VALUES (1,1),
+ (2,2),
+ (3,3);
+INSERT INTO "tb_studente" ("Matricola","Nome","Cognome","mail_istituzionale","Data_nascita","Anno_corso","Lingua_scelta","Password") VALUES (1,'Studente1','Cognome1','mail1','01/01/01',1,'lingua1','password1'),
+ (2,'Studente2','Cognome2','mail2','02/02/02',2,'lingua2','password2'),
+ (3,'Studente3','Cognome3','mail3','03/03/03',3,'lingua3','password3');
+INSERT INTO "tb_tutor" ("ID_tutor","Nome","Cognome","mail_interna","Password","Admin","Attivo","id_risorse") VALUES (NULL,'Tutor1','Cognome1','mail1','password1','true','true',1),
+ (NULL,'Tutor2','Cognome2','mail2','password2','false','true',2),
+ (NULL,'Tutor3','Cognome3','mail3','password3','false ','false',3);
 COMMIT;
