f6c933be5268fe3efe01fe3b8f72789daa50844e
Ilaria <sergiilaria06@gmail.com>
2024-02-16 10:30:53 +0100

    Aggiunto include iostream e dichiarate le funzioni

--- src/security/security.cpp
+++ src/security/security.cpp
@@ -1,15 +1,21 @@
+#include <iostream>
 #include <string>
 
+bool VerificaTutorAdmin () {
+}
+
+bool VerificaStudente () {
+}
 
 bool AutorizzazioneUtente () {
     // Richiedi email e la password
     // Se tutor/admin o Studente ritorna ok
     std :: string email ("");
     std :: string password ("");
-    cout << "Inserire email" << endl;
-    cin >> email;
-    cout << "Inserisci password" << endl;
-    cin >> password;
+    std :: cout << "Inserire email" << std :: endl;
+    std :: cin >> email;
+    std :: cout << "Inserisci password" << std :: endl;
+    std :: cin >> password;
     // Se sei tutor o admin ritorna true
     if (VerificaTutorAdmin()){
         return true;
