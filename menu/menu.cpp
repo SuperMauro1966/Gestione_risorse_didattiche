c44984a973077198289fde13f163bdbaf8a4ccdc
Ilaria <sergiilaria06@gmail.com>
2024-02-20 11:02:59 +0100

    Creata libreria di funzioni per la gestione dei menu

---
+++ menu/menu.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "security/security.hpp"
+
+void MostraMenu (utente_t){
+    std :: cout << "++Menu principale**" << std :: endl;
+}
+
+void MostraMenuTutor () {
+    std :: cout << "Menù Tutor: " << std :: endl;
+}
+
+void MostraMenuStudente () {
+    std :: cout << "Menù Studente: " << std :: endl;
+}
+
+void MostraMenuAdmin () {
+    std :: cout << "Menù Admin: " << std :: endl;
+}
